776ee6dd4b7367654c39b2340ee8bb83abc9717d
lizhen0821 <1337637096@qq.com>
2022-04-19 16:14:48 +0800

    去掉异常

--- src/main/java/com/example/lizhen/demo/config/RedissonConfig.java
+++ src/main/java/com/example/lizhen/demo/config/RedissonConfig.java
@@ -6,13 +6,12 @@ import org.redisson.config.Config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.IOException;
 
 @Configuration
 public class RedissonConfig {
     @Bean(destroyMethod="shutdown")
-    RedissonClient redisson() throws IOException {
-        //1、创建配置测试测试
+    RedissonClient redisson(){
+        //1、创建配置测试测试1asas
         Config config = new Config();
         config.useSingleServer()
                 .setAddress("redis://127.0.0.1:6379");
